237f84011c25201024aea844d93014591b8baaca
bizmyth <45700916+bizmythy@users.noreply.github.com>
2026-04-04 14:34:06 -0400

    Add NixOS module exposing t3code package (#4)
    - Refactor flake outputs to share per-system definitions
    - Export a `nixosModules.t3code` module that installs the default package

--- flake.nix
+++ flake.nix
@@ -26,53 +26,71 @@
       flake-utils,
       bun2nix,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ bun2nix.overlays.default ];
-        };
+    let
+      perSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ bun2nix.overlays.default ];
+          };
 
-        revision =
-          if self ? rev then self.rev else if self ? dirtyRev then self.dirtyRev else "unknown";
+          revision =
+            if self ? rev then
+              self.rev
+            else if self ? dirtyRev then
+              self.dirtyRev
+            else
+              "unknown";
 
-        t3code = pkgs.callPackage ./default.nix {
-          src = self;
-          inherit revision;
-        };
-        bun2nixCli = bun2nix.packages.${system}.default;
+          t3code = pkgs.callPackage ./default.nix {
+            src = self;
+            inherit revision;
+          };
+          bun2nixCli = bun2nix.packages.${system}.default;
 
-        t3codeApp = flake-utils.lib.mkApp {
-          drv = t3code;
-          name = "t3code";
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            (aspellWithDicts (ps: with ps; [ en ]))
-            # keep-sorted start
-            bun
-            nixfmt
-            nodejs_24
-            nushell
-            python3
-            # keep-sorted end
-            bun2nixCli
-          ];
-        };
-      }
-      // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-        packages = {
-          inherit t3code;
-          default = t3code;
-        };
+          t3codeApp = flake-utils.lib.mkApp {
+            drv = t3code;
+            name = "t3code";
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (aspellWithDicts (ps: with ps; [ en ]))
+              # keep-sorted start
+              bun
+              nixfmt
+              nodejs_24
+              nushell
+              python3
+              # keep-sorted end
+              bun2nixCli
+            ];
+          };
+        }
+        // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+          packages = {
+            inherit t3code;
+            default = t3code;
+          };
 
-        apps = {
-          t3code = t3codeApp;
-          default = t3codeApp;
-        };
-      }
-    );
+          apps = {
+            t3code = t3codeApp;
+            default = t3codeApp;
+          };
+        }
+      );
+    in
+    perSystem
+    // {
+      nixosModules = {
+        t3code =
+          { pkgs, ... }:
+          {
+            environment.systemPackages = [ self.packages.${pkgs.system}.default ];
+          };
+        default = self.nixosModules.t3code;
+      };
+    };
 }
